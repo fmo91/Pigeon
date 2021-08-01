f38aa12e8026a66f6bad9135496c27405ceebdcc
tiking76 <716yosiki1999@gmail.com>
2021-08-01 10:11:22 +0900

    fix warning

--- Example/Pigeon/AppDelegate.swift
+++ Example/Pigeon/AppDelegate.swift
@@ -14,14 +14,15 @@ import Pigeon
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         let controller = ViewController()
         window = UIWindow(frame: UIScreen.main.bounds)
-        window!.rootViewController = controller
-        window!.makeKeyAndVisible()
-        
+        if let window = window {
+            window.rootViewController = controller
+            window.makeKeyAndVisible()
+        }
         return true
     }
 }
-
--- Example/Tests/Tests.swift
+++ Example/Tests/Tests.swift
@@ -20,9 +20,8 @@ class Tests: XCTestCase {
     
     func testPerformanceExample() {
         // This is an example of a performance test case.
-        self.measure() {
+        self.measure {
             // Put the code you want to measure the time of here.
         }
     }
-    
 }
